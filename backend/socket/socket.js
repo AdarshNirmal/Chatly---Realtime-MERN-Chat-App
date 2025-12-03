ececf51a02e9b77d0b69bef1775c3919e95b6077
Adarsh Nirmal <mahakaaladi@gmail.com>
2025-12-03 13:40:23 +0530

    update

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -1,38 +1,52 @@
 import http from 'http'
 import express from 'express'
 import { Server } from 'socket.io'
-
-
+import Message from "../models/message.model.js"  
 
 let app = express()
 
 const server = http.createServer(app)
-const io  = new Server(server,{
-    cors:{
-        origin:["http://localhost:5173","https://chatly-realtime-mern-chat-app.onrender.com"],
-        credentials:true
-    }
+const io  = new Server(server, {
+  cors: {
+    origin: [
+      "http://localhost:5173",                              
+      "https://chatly-realtime-mern-chat-app.onrender.com"  
+    ],
+    credentials: true
+  }
 })
 
- const userSocketMap={}
- export const getReceiverSocketId=(receiver)=>{
-    return userSocketMap[receiver]
- }
-io.on("connection",(socket)=>{
-    const userId=socket.handshake.query.userId
-    if(userId!=undefined){
-        userSocketMap[userId]=socket.id
-    }
+const userSocketMap = {}                         
 
-io.emit("getOnlineUsers",Object.keys(userSocketMap))
-
-    socket.on("disconnect",()=>{
-       delete userSocketMap[userId]
-        io.emit("getOnlineUsers",Object.keys(userSocketMap))
+export const getReceiverSocketId = (receiver) => {  
+  return userSocketMap[receiver]
+}
 
 
-    })
+io.on("connection", async (socket) => {          
+  const userId = socket.handshake.query.userId
 
+  if (userId != undefined) {
+    userSocketMap[userId] = socket.id
+
+    
+    await Message.updateMany(
+      {
+        receiver: userId,        
+        status: "sent"          
+      },
+      { $set: { status: "delivered" } }
+    )
+  }
+
+  io.emit("getOnlineUsers", Object.keys(userSocketMap))
+
+  socket.on("disconnect", () => {
+    delete userSocketMap[userId]
+    io.emit("getOnlineUsers", Object.keys(userSocketMap))
+  })
 })
+
+
 
 export{app,server,io}
