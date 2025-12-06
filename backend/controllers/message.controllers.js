42e2844a6dd1b1f20e35e9832ea81e7ce18f04c3
Adarsh Nirmal <mahakaaladi@gmail.com>
2025-12-06 13:10:00 +0530

    update

--- backend/controllers/message.controllers.js
+++ backend/controllers/message.controllers.js
@@ -1,52 +1,58 @@
 import uploadOnCloudinary from "../config/clodinary.js";
 import Conversation from "../models/Conversation.model.js";
 import Message from "../models/message.model.js";
-import { getReceiverSocketId,io} from "../socket/socket.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
 
+// SEND MESSAGE
 export const sendMessage = async (req, res) => {
-    try {
-        let sender = req.userId
-        let { receiver } = req.params
-        let { message } = req.body
+  try {
+    const sender = req.userId
+    const { receiver } = req.params
+    const { message } = req.body
 
-        let image;
-        if (req.file) {
-            image = await uploadOnCloudinary(req.file.path)
-        }
-
-        let conversation = await Conversation.findOne({
-             partcipants:{$all:[sender,receiver]}
-        })
-
-        let newMessage=await Message.create({
-            sender,receiver,message,image
-        })
-        
-        if(!conversation){
-            conversation=await Conversation.create({
-                partcipants:[sender,receiver],
-                messages:[newMessage._id]
-            })
-        }else{
-            conversation.messages.push(newMessage._id)
-            await conversation.save()
-        }
-  
-        const receiverSocketId=getReceiverSocketId(receiver)
-        if(receiverSocketId){
-             newMessage.status = "delivered"
-             await newMessage.save()
-            io.to(receiverSocketId).emit("newMessage",newMessage)
-        }
-        
-
-       return res.status(201).json(newMessage)
-
-    } catch (error) {
-         return res.status(500).json({message:`send Message error{error}`})
+    let image
+    if (req.file) {
+      image = await uploadOnCloudinary(req.file.path)
     }
+
+    let conversation = await Conversation.findOne({
+      partcipants: { $all: [sender, receiver] }
+    })
+
+    let newMessage = await Message.create({
+      sender,
+      receiver,
+      message,
+      image
+      // status: "sent" default
+    })
+
+    if (!conversation) {
+      conversation = await Conversation.create({
+        partcipants: [sender, receiver],
+        messages: [newMessage._id]
+      })
+    } else {
+      conversation.messages.push(newMessage._id)
+      await conversation.save()
+    }
+
+    const receiverSocketId = getReceiverSocketId(receiver)
+    if (receiverSocketId) {
+      newMessage.status = "delivered"
+      await newMessage.save()
+      io.to(receiverSocketId).emit("newMessage", newMessage)
+    }
+
+    return res.status(201).json(newMessage)
+
+  } catch (error) {
+    console.log("send message error:", error)
+    return res.status(500).json({ message: `send Message error ${error.message}` })
+  }
 }
 
+// GET MESSAGES
 export const getMessage = async (req, res) => {
   try {
     const sender = req.userId
@@ -60,17 +66,16 @@ export const getMessage = async (req, res) => {
       return res.status(400).json({ message: "converstaion not found" })
     }
 
-   
+    // status -> seen
     await Message.updateMany(
       {
-        sender: receiver,         
-        receiver: sender,         
+        sender: receiver,
+        receiver: sender,
         status: { $in: ["sent", "delivered"] }
       },
       { $set: { status: "seen" } }
     )
 
-    
     conversation = await Conversation.findOne({
       partcipants: { $all: [sender, receiver] }
     }).populate("messages")
